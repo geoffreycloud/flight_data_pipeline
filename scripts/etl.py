ba7b2b1d8b1ba22827e03f66e30c041885cc3f2f
Geoffrey <gcloud@uncc.edu>
2025-10-30 13:57:36 -0400

    Load process complete

--- scripts/etl.py
+++ scripts/etl.py
@@ -5,6 +5,7 @@ from requests.exceptions import HTTPError
 import os
 from dotenv import load_dotenv
 import datetime
+import psycopg2
 
 load_dotenv()
 access_key = os.getenv('ACCESS_KEY')
@@ -13,8 +14,7 @@ access_key = os.getenv('ACCESS_KEY')
 url = 'https://www.goflightlabs.com/flights'
 
 def extract(url, access_key):
-    params = {'access_key': access_key,
-              'limit': 1000}
+    params = {'access_key': access_key}
 
     try:
         # Send a GET request to the API endpoint
@@ -25,11 +25,13 @@ def extract(url, access_key):
 
     except HTTPError as http_err:
         # Error occured on GET request
-        print(f'Connection error {http_err}')
+        print(f'HTTP error {http_err}')
     
     # Parse response content as JSON
     data = r.json()
     df = pd.json_normalize(data['data'])
+
+    print('Extraction complete')
     
     return df
 
@@ -41,11 +43,81 @@ def transform(df):
     default_vals = {'squawk': 'Unknown', 'alt': 0, 'speed': 0, 'v_speed': 0.0}
     df_copy = df_copy.fillna(default_vals)
 
+    # Removing duplicates
+    df_copy = df_copy.drop_duplicates(subset=['hex', 'updated'])
 
-    # Converting 'updated' values from timestamp to datetime
+    # Converting to imperial 
+    df_copy['alt'] = df_copy['alt'].apply(lambda x: x * 3.28084)
+    df_copy[['speed', 'v_speed']] = df_copy[['speed', 'v_speed']].apply(lambda x: x * 0.621371)
+
+
+    # Converting 'updated' values to datetime
     df_copy['updated'] = df_copy['updated'].apply(lambda x: datetime.datetime.fromtimestamp(x))
+
+    # Renaming columns
+    renamed_columns = {
+        'alt': 'altitude_ft', 'speed': 'speed_mph', 'v_speed': 'v_speed_mph', 'lat': 'latitude', 'lng': 'longitude',
+        'dep_icao': 'departure_icao', 'dep_iata': 'departure_iata', 'arr_icao': 'arrival_icao', 'arr_iata': 'arrival_iata'
+        }
+    
+    df_copy = df_copy.rename(columns=renamed_columns)
+
+    print('transform complete')
 
     return df_copy
 
-flight_data_raw = extract(url, access_key)
-flight_data_clean = transform(flight_data_raw)
+def load(df_raw, df_clean, conn):
+
+    cursor = conn.cursor()
+
+    # Inserting raw data into flights_real_time_raw table
+    
+    raw_cols = df_raw.columns
+
+    sql_raw = f"""
+    INSERT INTO flights_realtime_raw ({",".join(raw_cols)})
+    VALUES ({",".join(["%s"] * len(raw_cols))})
+    """
+
+    for _, row in df_raw[raw_cols].iterrows():
+        cursor.execute(sql_raw, tuple(row.values))
+
+    
+
+    
+    # Inserting clean data into flights_real_time_clean table
+    clean_cols = df_clean.columns
+
+    sql_clean = f"""
+    INSERT INTO flights_realtime_clean ({",".join(clean_cols)})
+    VALUES ({",".join(["%s"] * len(clean_cols))})
+    """
+
+    for _, row in df_clean[clean_cols].iterrows():
+        cursor.execute(sql_clean, tuple(row.values))
+
+
+    conn.commit()
+
+    print('load complete')
+
+if __name__ == "__main__":
+
+    # Extracting raw flight data from flight labs API
+    flight_data_raw = extract(url, access_key)
+
+    # Transforming raw flight data
+    flight_data_clean = transform(flight_data_raw)
+
+    # Connecting to local flight data database
+    conn = psycopg2.connect(
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
+    )
+
+    # Loading raw and clean data to Flight Data Database
+    load(flight_data_raw, flight_data_clean, conn)
+
